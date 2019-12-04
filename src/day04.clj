cd56e2e83e06930696b63dfa8461724a490c6e23
Immo Heikkinen <immo.heikkinen@zalando.fi>
2019-12-04 09:20:51 +0200

    Day 4 part 2

--- src/day04.clj
+++ src/day04.clj
@@ -15,3 +15,16 @@
 (defn part1 [x y]
   (count (filter meets-criteria? (range x (inc y)))))
 
+(defn same-digit-groups [n]
+  (partition-by identity (str n)))
+
+(defn has-same-digit-group-of-two? [groups]
+  (some #(= (count %) 2) groups))
+
+(defn meets-criteria2? [n]
+  (let [same-digit-groups (same-digit-groups n)]
+    (and (all-increasing-digits? (digit-pairs n))
+         (has-same-digit-group-of-two? same-digit-groups))))
+
+(defn part2 [x y]
+  (count (filter meets-criteria2? (range x (inc y)))))
