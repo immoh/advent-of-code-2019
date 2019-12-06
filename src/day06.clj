d6db3df7b6d70ed4d63d08c0e56e3b54ecf8dae9
Immo Heikkinen <immo.heikkinen@gmail.com>
2019-12-06 08:25:09 +0200

    Day 6 part 1

---
+++ src/day06.clj
@@ -0,0 +1,21 @@
+(ns day06
+  (:require
+    [clojure.string]))
+
+(defn parse-input [input]
+  (map (comp vec reverse #(clojure.string/split % #"\)")) (clojure.string/split-lines input)))
+
+
+(defn find-path* [relations path]
+  (if-let [next (get relations (last path))]
+    (recur relations (conj path next))
+    path))
+
+(defn find-path [relations from]
+  (find-path* relations [from]))
+
+(defn find-all-paths [orbits-relations]
+  (map (partial find-path orbits-relations) (keys orbits-relations)))
+
+(defn part1 [input]
+  (reduce + (map (comp count rest) (find-all-paths (into {} (parse-input input))))))
