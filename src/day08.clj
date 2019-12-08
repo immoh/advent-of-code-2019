abca91670c750b0a6209f661755de3bd7b8a7334
Immo Heikkinen <immo.heikkinen@gmail.com>
2019-12-08 09:18:11 +0200

    Day 8 part 2

--- src/day08.clj
+++ src/day08.clj
@@ -15,3 +15,23 @@
        (sort-by #(get % \0 0))
        (first)
        (checksum)))
+
+(defn combine-layers [width height layers]
+  (reduce
+    (fn [acc layer]
+      (map conj acc layer))
+    (repeat (* width height) [])
+    layers))
+
+(defn visible-pixel [pixels]
+  (first (drop-while #{\2} pixels)))
+
+(defn print-image [width image]
+  (doseq [row (partition width image)]
+    (println (clojure.string/replace (apply str row) "0" " "))))
+
+(defn part2 [width height input]
+  (->> (parse-input width height input)
+       (combine-layers width height)
+       (map visible-pixel)
+       (print-image width )))
