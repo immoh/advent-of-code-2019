44a01c6b28b7bb1a209a6367c129d683dc9127f6
Immo Heikkinen <immo.heikkinen@gmail.com>
2019-12-10 07:41:37 +0200

    WIP

---
+++ src/day10.clj
@@ -0,0 +1,14 @@
+(ns day10
+  (:require
+    [clojure.string]))
+
+(defn parse-input [input]
+  (map-indexed (fn [y row]
+                 (map-indexed (fn [y c] )
+                              row
+                              )
+
+                 ) (clojure.string/split-lines input)))
+
+
+(defn part1 [input])
