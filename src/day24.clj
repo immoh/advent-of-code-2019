3f143e691e07ab08dadae42dc273a0b1f85efaf6
Immo Heikkinen <immo.heikkinen@zalando.fi>
2019-12-24 15:59:54 +0200

    Day 24 part 1

--- src/day24.clj
+++ src/day24.clj
@@ -46,3 +46,69 @@
 
 (defn part1 [input]
   (find-cycle (parse-input input)))
+
+(defn resolve-neighbor [{:keys [level xy]} direction]
+  (let [[neighbor-x neighbor-y :as neighbor-xy] (mapv + xy direction)]
+    (cond
+      (= neighbor-xy [2 2])
+      (case direction
+        [0 1] [{:level (inc level) :xy [0 0]}
+               {:level (inc level) :xy [1 0]}
+               {:level (inc level) :xy [2 0]}
+               {:level (inc level) :xy [3 0]}
+               {:level (inc level) :xy [4 0]}]
+        [0 -1] [{:level (inc level) :xy [0 4]}
+                {:level (inc level) :xy [1 4]}
+                {:level (inc level) :xy [2 4]}
+                {:level (inc level) :xy [3 4]}
+                {:level (inc level) :xy [4 4]}]
+        [1 0] [{:level (inc level) :xy [0 0]}
+               {:level (inc level) :xy [0 1]}
+               {:level (inc level) :xy [0 2]}
+               {:level (inc level) :xy [0 3]}
+               {:level (inc level) :xy [0 4]}]
+        [-1 0] [{:level (inc level) :xy [4 0]}
+                {:level (inc level) :xy [4 1]}
+                {:level (inc level) :xy [4 2]}
+                {:level (inc level) :xy [4 3]}
+                {:level (inc level) :xy [4 4]}])
+
+      (= neighbor-x -1)
+      [{:level (dec level) :xy [1 2]}]
+
+      (= neighbor-x 5)
+      [{:level (dec level) :xy [3 2]}]
+
+      (= neighbor-y -1)
+      [{:level (dec level) :xy [2 1]}]
+
+      (= neighbor-y 5)
+      [{:level (dec level) :xy [2 3]}]
+
+      :else
+      [{:level level :xy neighbor-xy}])))
+
+(defn neighbors2 [position]
+  (mapcat (partial resolve-neighbor position) [[0 1] [0 -1] [1 0] [-1 0]]))
+
+(defn bug2? [bugs position]
+  (let [neighbor-bug-count (count (filter bugs (neighbors2 position)))]
+    (or (and (bugs position)
+             (= 1 neighbor-bug-count))
+        (and (not (bugs position))
+             (#{1 2} neighbor-bug-count)))))
+
+(defn next-minute2 [bugs]
+  (let [levels (map :level bugs)]
+    (set (filter (partial bug2? bugs) (for [level (range (dec (reduce min levels)) (+ 2 (reduce max levels)))
+                                           y (range 5)
+                                           x (range 5)
+                                           :when (not= [x y] [2 2])]
+                                       {:level level :xy [x y]})))))
+
+(defn parse-input2 [input]
+  (set (map (fn [xy] {:level 0 :xy xy}) (parse-input input))))
+
+;; too low 983
+(defn part2 [input]
+  (count (first (drop 200 (iterate next-minute2 (parse-input2 input))))))
